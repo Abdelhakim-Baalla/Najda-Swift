09475c6b221ae40503e352614f36815cea0bf39b
Abdelhakim Baalla <abdelhakimbaalla50@gmail.com>
2025-11-26 08:58:58 +0100

    feat: Initialize React app rendering and Redux Toolkit store setup.

---
+++ src/store/index.ts
@@ -0,0 +1,20 @@
+import { configureStore, createSlice } from '@reduxjs/toolkit';
+
+const placeholderSlice = createSlice({
+    name: 'placeholder',
+    initialState: { value: 0 },
+    reducers: {
+        increment: (state) => {
+            state.value += 1;
+        },
+    },
+});
+
+export const store = configureStore({
+    reducer: {
+        placeholder: placeholderSlice.reducer,
+    },
+});
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;  
