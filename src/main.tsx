c29e57eed954b1e064cd277f586d5c139153da46
Abdelhakim Baalla <abdelhakimbaalla50@gmail.com>
2025-11-26 09:12:34 +0100

    feat: Initialize React application with Redux and React Query providers.

--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,14 @@ import './index.css'
 import App from './App.tsx'
 import { Provider } from 'react-redux';
 import { store } from './store';
+import { QueryProvider } from './providers/QueryProvider';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <QueryProvider>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </QueryProvider>
   </StrictMode>,
 )
